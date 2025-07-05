a61aaaa3293720ec6b493da08a4494f00eb3ead8
Nevin-Bali100 <opopopopop100000@gmail.com>
2025-07-05 18:33:47 +0530

    Advanced Numpy

--- Advanced_Numpy/adv4.py
+++ Advanced_Numpy/adv4.py
@@ -1,1 +1,12 @@
 #Stacking :
+
+'''horizontal Stacking : --> hstack()'''
+'''vertical Stacking : --> vstack()'''
+
+import numpy as np
+
+arr1 = np.array([1,2,3])
+arr2 = np.array([4,5,6])
+
+print("Vertical Stacking :",np.vstack((arr1,arr2)))
+print("Horizontal Stacking :",np.hstack((arr1,arr2)))
